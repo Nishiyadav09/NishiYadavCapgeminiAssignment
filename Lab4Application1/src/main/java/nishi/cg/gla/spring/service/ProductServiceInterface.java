9e052dbc5bf6ddb4f8922d79785f25cf08bca01f
Manu Yadav <Manu Yadav@DESKTOP-0K954LC>
2020-04-30 16:02:28 +0530

    commit

---
+++ Lab4Application1/src/main/java/nishi/cg/gla/spring/service/ProductServiceInterface.java
@@ -0,0 +1,15 @@
+package nishi.cg.gla.spring.service;
+
+import java.util.List;
+
+
+import nishi.cg.gla.spring.entity.Product;
+
+
+public interface ProductServiceInterface {
+	
+	public Product save(Product product);
+
+	public List<Product> reterive();
+
+}
